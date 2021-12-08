489907c616019961f45ac7d0461d4029798d8f90
spelui <vlad.spelui@gmail.com>
2021-12-08 22:37:01 +0200

    add

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Statistics = ({ title }) => {
   return (
     <section className={s.statistics}>
-      <h2 className={s.title}>{title}</h2>
+      {title && <h2 className={s.title}>{title}</h2>}
 
       <ul className={s.statList}>
         <StatisticsItem />
